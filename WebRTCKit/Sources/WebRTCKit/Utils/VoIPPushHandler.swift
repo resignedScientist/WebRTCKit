168c8f1167d25e2ad9f278d3bf04c3654017a4d1
Norman Laudien <kontakt@norman-laudien.de>
2026-04-09 12:19:57 +0700

    make VoIPPushHandlerDelegate sendable

--- WebRTCKit/Sources/WebRTCKit/Utils/VoIPPushHandler.swift
+++ WebRTCKit/Sources/WebRTCKit/Utils/VoIPPushHandler.swift
@@ -5,6 +5,6 @@ public protocol VoIPPushHandler: Sendable {
     func setDelegate(_ delegate: VoIPPushHandlerDelegate)
 }
 
-public protocol VoIPPushHandlerDelegate: AnyObject {
+public protocol VoIPPushHandlerDelegate: AnyObject, Sendable {
     func didReceivePushNotification(payload: PushPayload) async
 }
